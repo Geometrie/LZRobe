1a1b554c99174af01aeeb52bc312b629a8ec3aa9
Geometrie <48816726+Geometrie@users.noreply.github.com>
2019-05-29 20:38:15 +0800

    Update International.h

--- International.h
+++ International.h
@@ -1,150 +1,150 @@
 #ifndef INTERNATIONAL_H
 #define INTERNATIONAL_H
-#define GRKLT_ALPHA "��"
-#define GRKLT_BETA "��"
-#define GRKLT_GAMMA "��"
-#define GRKLT_DELTA "��"
-#define GRKLT_EPSILON "��"
-#define GRKLT_ZETA "��"
-#define GRKLT_ETA "��"
-#define GRKLT_THETA "��"
-#define GRKLT_IOTA "��"
-#define GRKLT_KAPPA "��"
-#define GRKLT_LAMBDA "��"
-#define GRKLT_MU "��"
-#define GRKLT_NU "��"
-#define GRKLT_XI "��"
-#define GRKLT_OMICRON "��"
-#define GRKLT_PI "��"
-#define GRKLT_RHO "��"
-#define GRKLT_SIGMA "��"
-#define GRKLT_TAU "��"
-#define GRKLT_UPSILON "��"
-#define GRKLT_PHI "��"
-#define GRKLT_KI "��"
-#define GRKLT_PSI "��"
-#define GRKLT_OMEGA "��"
-#define LANGUAGE_CHINESE_SIMPLIFIED
+#define GRKLT_ALPHA "α"
+#define GRKLT_BETA "β"
+#define GRKLT_GAMMA "γ"
+#define GRKLT_DELTA "δ"
+#define GRKLT_EPSILON "ε"
+#define GRKLT_ZETA "ζ"
+#define GRKLT_ETA "η"
+#define GRKLT_THETA "θ"
+#define GRKLT_IOTA "ι"
+#define GRKLT_KAPPA "κ"
+#define GRKLT_LAMBDA "λ"
+#define GRKLT_MU "μ"
+#define GRKLT_NU "ν"
+#define GRKLT_XI "ξ"
+#define GRKLT_OMICRON "ο"
+#define GRKLT_PI "π"
+#define GRKLT_RHO "ρ"
+#define GRKLT_SIGMA "σ"
+#define GRKLT_TAU "τ"
+#define GRKLT_UPSILON "υ"
+#define GRKLT_PHI "φ"
+#define GRKLT_KI "χ"
+#define GRKLT_PSI "ψ"
+#define GRKLT_OMEGA "ω"
+//#define LANGUAGE_CHINESE_SIMPLIFIED
 #ifdef LANGUAGE_CHINESE_SIMPLIFIED
-#define STR_NEW "�¶Ծ�"
-#define STR_HANDICAP "���ӶԾ�"
-#define STR_HANDICAP_NUM "����������"
-#define STR_OPEN "��"
-#define STR_SAVE "����"
-#define STR_EXIT "�˳�"
-#define STR_BACKWARD "����"
-#define STR_FORWARD "����"
-#define STR_SELECT_ENGINE "ѡ������"
-#define STR_SELECT_WEIGHT "ѡ��Ȩ��"
-#define STR_EXTRA_PARA "���Ӳ���"
-#define STR_BOARD_SIZE "�ı����̴�С"
-#define STR_RUN_ENGINE "��������"
-#define STR_CLOSE_ENGINE "�ر�����"
-#define STR_RESULT "����ʤ��"
-#define STR_STEP "��ʾ�岽���"
-#define STR_COORDINATE "��������"
-#define STR_COORD_NULL "��������"
-#define STR_COORD_NET "����ͻ�������"
-#define STR_COORD_NUM "����������"
-#define STR_COORD_SGF "SGF��ĸ����"
-#define STR_COORD_GTP "GTPЭ������"
-#define	STR_SET_GAMEBOARD_COLOR "����������ɫ"
-#define STR_MANUAL "ʹ��˵��"
-#define STR_ABOUT "����LZRobe"
-#define STR_FILE "�ļ�"
-#define STR_EDIT "�༭"
-#define STR_VIEW "��ͼ"
-#define STR_LINK "����"
-#define STR_HELP "����"
-#define STR_BLACK_DOG "AIִ��"
-#define STR_WHITE_DOG "AIִ��"
-#define STR_TIME "˼��ʱ��"
-#define STR_SHOW_ANALYZE "��ʾ����"
-#define STR_INTERVAL "ˢ�¼��"
-#define STR_ROUTE_LENGTH "Ԥ�ⳤ��"
-#define STR_NO_ENGINE "����δ����"
-#define STR_TIP "��ʾ"
-#define STR_ADD_BRANCH_INQUIRY "ȷ�����ӷ�֧ô��"
-#define STR_DELETE_BRANCH_INQUIRY "ȷ��ɾ����֧ô��"
-#define STR_CURRENT_SITUATION "��ǰ����"
-#define STR_BLACK_RESIGN "�����䣬������ʤ"
-#define STR_WHITE_RESIGN "�����䣬������ʤ"
-#define STR_BLACK_OWN "������%.1f"
-#define STR_WHITE_OWN "������%.1f"
-#define STR_DISCARD_RECORD_INQUIRY "��⵽�Ծ���Ϣ��ȷ��Ҫ����ô��"
-#define STR_EXTRA_PARAMETERS "������������"
-#define STR_CLOSE_ENGINE_INQUIRY "ȷ���ر�����ô��"
-#define STR_CHANGE_PATH "��⵽���޸����������Ȩ�ص�·�����Ƿ񱣴棿"
-#define STR_CHANGE_EXTRA_PARA "��⵽���������˶���������Ƿ񱣴棿"
-#define STR_RESTART_ENGINE "�����������������"
-#define STR_ENGINE_OPENING "�������������������ĵȴ�"
-#define STR_ENGINE_AVAILABLE "����������"
-#define STR_ENGINE_CLOSING "�������ڹر�"
-#define STR_SELFPLAY "�����������Լ��Ծ�"
-#define STR_ANALYZING "���Ʒ�����"
-#define STR_VERSION "��ǰ�汾��1.03"
-#define STR_CLOSE "�ر�"
-#define STR_MOVE_TURN "����Ȩ"
-#define STR_PRISONERS "����"
-#define STR_PASS "����һ��"
-#define STR_RESIGN "����"
-#define STR_GAME_PROCESS "�Ծֽ���"
-#define STR_DIRECTION "��ӭ����LZRobe������һ��Ϊ�˹�����Χ������LeelaZero�ṩͼ�ν���ĳ���\n\n\
-��������˽����LeelaZero�������Ϣ��������������ַ��\n\n\
+#define STR_NEW "新对局"
+#define STR_HANDICAP "让子对局"
+#define STR_HANDICAP_NUM "设置让子数"
+#define STR_OPEN "打开"
+#define STR_SAVE "保存"
+#define STR_EXIT "退出"
+#define STR_BACKWARD "撤消"
+#define STR_FORWARD "重做"
+#define STR_SELECT_ENGINE "选择引擎"
+#define STR_SELECT_WEIGHT "选择权重"
+#define STR_EXTRA_PARA "附加参数"
+#define STR_BOARD_SIZE "改变棋盘大小"
+#define STR_RUN_ENGINE "启动引擎"
+#define STR_CLOSE_ENGINE "关闭引擎"
+#define STR_RESULT "计算胜负"
+#define STR_STEP "显示棋步序号"
+#define STR_COORDINATE "棋盘坐标"
+#define STR_COORD_NULL "隐藏坐标"
+#define STR_COORD_NET "网棋客户端坐标"
+#define STR_COORD_NUM "纯数字坐标"
+#define STR_COORD_SGF "SGF字母坐标"
+#define STR_COORD_GTP "GTP协议坐标"
+#define	STR_SET_GAMEBOARD_COLOR "设置棋盘颜色"
+#define STR_MANUAL "使用说明"
+#define STR_ABOUT "关于LZRobe"
+#define STR_FILE "文件"
+#define STR_EDIT "编辑"
+#define STR_VIEW "视图"
+#define STR_LINK "连接"
+#define STR_HELP "帮助"
+#define STR_BLACK_DOG "AI执黑"
+#define STR_WHITE_DOG "AI执白"
+#define STR_TIME "思考时间"
+#define STR_SHOW_ANALYZE "显示分析"
+#define STR_INTERVAL "刷新间隔"
+#define STR_ROUTE_LENGTH "预测长度"
+#define STR_NO_ENGINE "引擎未运行"
+#define STR_TIP "提示"
+#define STR_ADD_BRANCH_INQUIRY "确定增加分支么？"
+#define STR_DELETE_BRANCH_INQUIRY "确定删除分支么？"
+#define STR_CURRENT_SITUATION "当前局势"
+#define STR_BLACK_RESIGN "黑认输，白中盘胜"
+#define STR_WHITE_RESIGN "白认输，黑中盘胜"
+#define STR_BLACK_OWN "黑领先%.1f"
+#define STR_WHITE_OWN "白领先%.1f"
+#define STR_DISCARD_RECORD_INQUIRY "检测到对局信息，确定要丢弃么？"
+#define STR_EXTRA_PARAMETERS "输入其它参数"
+#define STR_CLOSE_ENGINE_INQUIRY "确定关闭引擎么？"
+#define STR_CHANGE_PATH "检测到你修改了引擎或者权重的路径，是否保存？"
+#define STR_CHANGE_EXTRA_PARA "检测到了你增加了额外参数，是否保存？"
+#define STR_RESTART_ENGINE "引擎崩溃，正在重启"
+#define STR_ENGINE_OPENING "引擎正在启动，请耐心等待"
+#define STR_ENGINE_AVAILABLE "引擎已启动"
+#define STR_ENGINE_CLOSING "引擎正在关闭"
+#define STR_SELFPLAY "引擎正在与自己对局"
+#define STR_ANALYZING "局势分析中"
+#define STR_VERSION "当前版本：1.03"
+#define STR_CLOSE "关闭"
+#define STR_MOVE_TURN "着子权"
+#define STR_PRISONERS "提子"
+#define STR_PASS "放弃一手"
+#define STR_RESIGN "认输"
+#define STR_GAME_PROCESS "对局进程"
+#define STR_DIRECTION "欢迎试用LZRobe！这是一个为人工智能围棋软件LeelaZero提供图形界面的程序。\n\n\
+如果你想了解关于LeelaZero更多的信息，请访问下面的网址：\n\n\
 zero.sjeng.org\n\n\
-LZRobe���԰�������LeelaZero��ս�����߷���������������ˮƽ��\n\n\
-LZRobe�ǻ���C++����д�ģ���ʹ����һ����wxWidgets��ͼ�ν���⡣\n\n\
-��������������LZRobe������Ҫ�����ز�����wxWidgets�⣬��������ڵ���ַ��\n\n\
+LZRobe可以帮助你与LeelaZero对战，或者分析棋谱来提高你的水平。\n\n\
+LZRobe是基于C++语言写的，并使用了一个叫wxWidgets的图形界面库。\n\n\
+所以如果你想编译LZRobe，你需要先下载并配置wxWidgets库，请浏览下在的网址：\n\n\
 www.wxwidgets.org\n\n\
-ʹ��Visual Studio����Code::Blocks����LZRobe�������ѡ�\n\n\
-����������ű���LZRobe������Ҫ����һ������Win32ͼ���û����������Ŀ��Ȼ���.h�ļ���.cpp�ļ���һ��.rc�ļ���һ��.ico�ļ���.bmp�ļ�������ȥ��\n\n\
-�ǵ��ڹ��������а�wxWidgets�İ����ļ��Ϳ��ļ���·���ӽ�ȥ��\n\n\
-Code::Blocks֧�ִ���һ��wxWidgets��Ŀ���Զ�����wxWidgets�����·��������Ҳ�����Լ򻯱������̡�\n\n\
-����ɹ�����ó����Ժ󣬸����������LZRobe��\n\n\
-LZRobe��ͼ����һ������ͷ����ΪΧ���Ӣ�ġ�Go���ķ���������������ġ�������\n\n\
-�����LZRobe�Ժ���ῴ���˵���������һ������������������ܶ������ڹ��������ҵ���\n\n\
-�¶Ծֻ�ɾ����ǰ�ĶԾ���Ϣ�����ӶԾ�ֻ�����ļ��˵�����ѡȡ��\n\n\
-LZRobe����������sgf�ļ��Ϸŵ����������򿪡�Ҳ���԰�.sgf��ʽ�������ļ��Ĵ򿪷�ʽֱ�����ó�LZRobe��\n\n\
-���������ʾ������һ���������������Ҳ��з���һ���������ѡ�㡣�������·���״̬������ʾ�����״̬��\n\n\
-���������������ӣ�����Ҽ��������壬�������µĶԾ����̿���������ת�������岽��\n\n\
-�Ծ����̰��մ����ҵ�˳���������У�ÿһ����25���壬����ת����Ӧ���岽��ʹ������������Ӧ��λ�ü��ɡ�\n\n\
-Ϊ�˱�������������ѵ���LeelaZero����������������濪���ȴ���ʱ���ڣ��������ת�岽�ǲ������ģ����������ӡ�\n\n\
-LZRobe֧�ֶ��֧���ף������ı���ԭ�������е��·���������ʾ����ѡ���Ƿ������µķ�֧�������ױ���Ϊsgf��ʽҲ����ͬʱ�������еķ�֧��\n\n\
-Ϊ�˲������ۻ����ң����֧�����ڶԾ�������ֻ����ʾ��ǰ�岽���ڵ�����������з�֧���岽��ֱ���ԭ���ĺ�ɫ���ɫ��Ϊ��ɫ����ɫ��\n\n\
-�������ת���з�֧���岽������һ���Ĳ�ͬ��֧�ᰴ�����ð���ɫ�����дӢ����ĸ��ע�������ϡ�A��������ķ�֧��Խ�̵ķ�֧��Ӧ��ĸ��Խ�����λ�õ���ĸ��\n\n\
-�������������Ӧ��ĸ���ڵ�λ�ÿ��Խ����Ӧ�ķ�֧��������Ҽ���������ɾ����Ӧ�ķ�֧��\n\n\
-���֧����˳���Ǳ��ֶ�̬ά���ģ���������ӻ���ɾ���˷�֧�����ܻ��������һ����֧���������Ӱ�졣\n\n\
-����Leela Zero�Ĺٷ�Ȩ�ز�֧�ַ�19·�Ծ֣����Է�19·������ֻ֧�ִ򿪡��޸ĺͱ������ס����Ҫ�򿪷�19·���ף���Ҫ�Ȱ����̵�������Ӧ��С��\n\n\
-�����������ѡ�����أ�Ҳ����ѡ���ֲ�ͬ����ʾģʽ��ע��GTPЭ������ĺ�����û����ĸ��I�����������Ǵ����������еġ�\n\n\
-����������һ�����а�ť������ʹ�������������LeelaZero��\n\n\
-�����û��ΪLeelaZero��������·������Ȩ���ļ������жԻ��򵯳���ʾ��ѡ���ִ���ļ���Ȩ���ļ���\n\n\
-������ʹ������������棬���綯̬��Ŀ���㲻��Ҫ��������������\n\n\
-��LeelaZero�ɹ������Ժ󣬵������룬��������ļ�����ɫ��ť���ɲ�ɫ��������а�ť�ɱ�������ر�����İ�ť��\n\n\
-�����������Ϸ�����иı��������Ȩ���ļ�������Ҫ�ȹر����棬Ȼ���ڱ༭�˵����޸������Ȩ���ļ�·��������������漴�ɡ�\n\n\
-��������������Զ����ص�ǰ�ĶԾֽ��ȡ���ʹLeelaZero���������LZRobeҲ����������������������Զ����ضԾֽ�����ȷ���Ծֵ�˳�����С�\n\n\
-������ڳ��������Ժ���Ҫ���������ӣ���ǰ�ı仯���ᱻ���档\n\n\
-���Ź���ͷ��İ�ť������LeelaZero�Ծ֡�����ͷ�����ı�����ʾAI��ִ���ӵ���ɫ��\n\n\
-��������������ͷ��İ�ť�����£���AI�����ҶԾ֡�\n\n\
-�ڡ�˼��ʱ�䡱��ǩ�ұߵ����ֿ��ʾAI˼��ʱ�����ã�ʱ�䵥λ��һ�롣\n\n\
-���磬����������������ó���5����AI���ڶ������Ӻ��5��֮���³�һ���塣\n\n\
-�����ı���˼����ʱ�䣬���������´���AI����ʱ��Ч��\n\n\
-�������AI˼���ڼ�ȡ��������ִ��һ���������ǻ����˼����ֱ���³���һ���塣�����㻹��Ҫ�ȵ���һ�����������ϳ��֡�\n\n\
-�����ʺŵİ�ť�����������ף����������һ����Ƶ��ˢ�¡�\n\n\
-ˢ��Ƶ�ʿ������á�ˢ�¼������ǩ�ұߵ����ֿ������ã���λ�ǰٷ�֮һ�롣\n\n\
-���磬�����������ϳ���50���Ƿ����������1����ˢ�����Ρ�\n\n\
-�ڷ��������У��̵����ɫ���������ĵ㣬��������㣬�Ƶ�ͺ����������١�\n\n\
-��ɫ���ϵ����ֱ�ʾʤ�ʺ������������������ĵ�ͨ������õ�ѡ�㡣\n\n\
-���������ŵ����ڷ����ĵ��ϣ����������ķ������������Զ���ʾ����������ĸ��ʾ˳��\n\n\
-������СдӢ����ĸ��Ȼ����Сдϣ����ĸ����ʾ������������á�Ԥ�ⳤ�ȡ���ǩ�ұߵ����ֿ������á�\n\n\
-������������ַ���(˫����һ��)����һ�����䣬����ֽ�����LeelaZero����Խ����������������\n\n\
-�����������ѽ�������֣���Ҫ��ͷ���أ����׵���Ϣ����ı䡣\n\n\
-�ڶԾֽӽ���������һ��ռ�о��Ե����Ƶ�ʱ��LeeaZero���ܻ��з����Ĳ���ѡ�㣬�鿴���ѡ��ķ������԰������ڷ���ѡ���ѡ���ϡ�\n\n\
-�������̱�ʶ�İ�ť��������Ծֽ�����������Ӳ�һ���������������ʾ�������׼ȷ��\n\n\
-�����ı���LeelaZero�������Ȩ���ļ�·�������߸ı��˸��Ӳ����������˳�LZRobe��ʱ�򣬻��жԻ��������Ƿ񱣴档\n\n\
-�����ѡ���˱�������仯����LeelaZero�������Ȩ���ļ�·���Լ���ĸ��Ӳ����ᱻ���浽һ����Ϊ��config.txt�����ļ��У��´��������Զ����ء�\n\n\
-��Ȼ���������һֱ��Ŭ�����ٳ����Bug����������Ȼ�޷�ȷ�����򲻻������\n\n\
-����������������LeelaZero��Ȼ�����У���ʹ���������������LeelaZero������������ռ�����Ӳ����Դ��\n\n\
-��л��������������ļ�����ӭ�Գ���������Ĥ�ģ�����Ӧ����ĳ��ϣ�"
+使用Visual Studio或者Code::Blocks编译LZRobe并不困难。\n\n\
+如果你想试着编译LZRobe，首先要建立一个基于Win32图形用户界面的项项目，然后把.h文件、.cpp文件，一个.rc文件，一个.ico文件和.bmp文件包含进去。\n\n\
+记得在工程设置中把wxWidgets的包含文件和库文件的路径加进去。\n\n\
+Code::Blocks支持创建一个wxWidgets项目，自动包含wxWidgets的相关路径，这样也许可以简化编译流程。\n\n\
+在你成功编译好程序以后，告诉你如何用LZRobe。\n\n\
+LZRobe的图标是一个狗的头像，因为围棋的英文“Go”的发音类似于中文里的“狗”。\n\n\
+在你打开LZRobe以后，你会看到菜单栏下面有一个工具栏，大多数功能都可以在工具栏中找到。\n\n\
+新对局会删除当前的对局信息。让子对局只能在文件菜单栏中选取。\n\n\
+LZRobe允许用鼠标把sgf文件拖放到窗口中来打开。也可以把.sgf格式的棋谱文件的打开方式直接设置成LZRobe。\n\n\
+棋盘左侧显示了落子一方和提子数量，右侧有放弃一步和认输的选点。主窗口下方的状态栏会显示引擎的状态。\n\n\
+鼠标左键用来放棋子，鼠标右键用来悔棋，键盘右下的对局流程可以用来跳转到任意棋步。\n\n\
+对局流程按照从左到右的顺序蛇形排列，每一列有25手棋，想跳转到对应的棋步，使用鼠标左键点相应的位置即可。\n\n\
+为了避免出现命令扎堆导致LeelaZero崩溃的情况，在引擎开启等待的时间内，悔棋和跳转棋步是不允许的，但可以落子。\n\n\
+LZRobe支持多分支棋谱，如果你改变了原来棋谱中的下法，会有提示让你选择是否增加新的分支。把棋谱保存为sgf格式也可以同时存下所有的分支。\n\n\
+为了不至于眼花缭乱，多分支棋谱在对局流程中只会显示当前棋步所在的最长链，其中有分支的棋步会分别由原来的黑色与白色变为红色与蓝色。\n\n\
+如果你跳转到有分支的棋步，那下一步的不同分支会按长短用暗紫色粗体大写英文字母标注在棋盘上。A代表着最长的分支，越短的分支对应字母表越靠后的位置的字母。\n\n\
+用鼠标左键点击相应字母所在的位置可以进入对应的分支，用鼠标右键点击则可以删除相应的分支。\n\n\
+多分支链的顺序是保持动态维护的，如果你增加或者删除了分支，可能会对它的上一级分支的排序产生影响。\n\n\
+由于Leela Zero的官方权重不支持非19路对局，所以非19路的棋盘只支持打开、修改和保存棋谱。如果要打开非19路棋谱，需要先把棋盘调整到相应大小。\n\n\
+棋盘坐标可以选择隐藏，也可以选择几种不同的显示模式。注意GTP协议坐标的横坐标没有字母“I”，纵坐标是从下往上排列的。\n\n\
+工具栏里有一个运行按钮，可以使用这个按键启动LeelaZero。\n\n\
+如果你没有为LeelaZero设置引擎路径或者权重文件，会有对话框弹出提示你选择可执行文件和权重文件。\n\n\
+除非你使用了特殊的引擎，比如动态贴目，你不需要输入其它参数。\n\n\
+当LeelaZero成功启动以后，等上数秒，工具栏里的几个灰色按钮会变成彩色，这个运行按钮由变成用来关闭引擎的按钮。\n\n\
+如果你想在游戏过程中改变引擎或者权重文件，你需要先关闭引擎，然后在编辑菜单中修改引擎和权重文件路径，最后重启引擎即可。\n\n\
+新启动的引擎会自动加载当前的对局进度。即使LeelaZero意外崩溃，LZRobe也会对它重启，并在数秒内自动加载对局进度以确保对局的顺利进行。\n\n\
+如果你在撤消几步以后想要新增加棋子，以前的变化不会被保存。\n\n\
+带着狗的头像的按钮用来和LeelaZero对局。狗的头像后面的背景表示AI所执棋子的颜色。\n\n\
+如果你把两个狗的头像的按钮都按下，那AI会自我对局。\n\n\
+在“思考时间”标签右边的数字框表示AI思考时间设置，时间单位是一秒。\n\n\
+例如，如果你把这个数字设置成了5，那AI会在对手下子后的5秒之内下出一步棋。\n\n\
+如果你改变了思考的时间，它会在你下次让AI走棋时生效。\n\n\
+如果你在AI思考期间取消了它所执的一方，它还是会继续思考，直到下出下一步棋。所以你还是要等到下一步棋在棋盘上出现。\n\n\
+带着问号的按钮用来分析棋谱，分析结果按一定的频率刷新。\n\n\
+刷新频率可以在用“刷新间隔”标签右边的数字框来设置，单位是百分之一秒。\n\n\
+例如，你把这个数字认成了50，那分析结果会在1秒内刷新两次。\n\n\
+在分析过程中，绿点的颜色是搜索最多的点，其次是蓝点，黄点和红点的搜索较少。\n\n\
+彩色点上的数字表示胜率和搜索数。搜索数最多的点通常是最好的选点。\n\n\
+如果你把鼠标放到正在分析的点上，这个点下面的分析步数将会自动显示，上面有字母表示顺序。\n\n\
+首先是小写英文字母，然后是小写希腊字母。显示的最大步数可以用“预测长度”标签右边的数字框来设置。\n\n\
+如果有连续两手放弃(双方各一手)或者一方认输，则棋局结束，LeelaZero不会对结束的棋局做分析。\n\n\
+如果你想分析已结束的棋局，需要从头加载，棋谱的信息不会改变。\n\n\
+在对局接近结束或者一方占有绝对的优势的时候，LeeaZero可能会有放弃的步的选点，查看这个选点的分析可以把鼠标放在放弃选项的选点上。\n\n\
+带着算盘标识的按钮用来计算对局结果。由于死子不一定被提掉，所以显示结果并不准确。\n\n\
+如果你改变了LeelaZero的引擎和权重文件路径，或者改变了附加参数，当你退出LZRobe的时候，会有对话框问你是否保存。\n\n\
+如果你选择了保存这个变化，那LeelaZero的引擎和权重文件路径以及你的附加参数会被保存到一个名为“config.txt”的文件中，下次启动会自动加载。\n\n\
+虽然程序的作者一直在努力减少程序的Bug，但我们依然无法确保程序不会崩溃。\n\n\
+如果程序意外崩溃，LeelaZero依然在运行，请使用任务管理器结束LeelaZero，以免它继续占用你的硬件资源！\n\n\
+感谢您读完这个帮助文件。欢迎对程序代码进行膜改，以适应更多的场合！"
 #else
 #define STR_NEW "new"
 #define STR_HANDICAP "handicap game"
